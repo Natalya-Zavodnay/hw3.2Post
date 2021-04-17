be7134b722904c49ae133370a3165af2d49088fd
Natalya-Zavodnay <nz41330@gmail.com>
2021-04-17 10:19:34 +0300

    added Manager commit

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,21 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private PostManager[] postManagers;
+
+    public PostManager[] generateBlock() {
+        return null;
+    }
+
+    public Post[] search() {
+        return null;
+    }
+
+
+    public void delete() {
+
+
+    }
+}
